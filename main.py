880ab5225561365d25c695ea2975be5d02ac9598
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-09-28 15:02:13 +0300

    Testing regulars for getting id

--- main.py
+++ main.py
@@ -44,18 +44,13 @@ path = os.path.join(os.getcwd() + '/videos/')
 
 
 def get_video_id(url: str):
-    query = urlparse(url)
-    if query.hostname == 'youtu.be':
-        return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
-            p = urlparse.parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
-    return url
+    regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
+
+    match = regex.match(self.youtube_url)
+
+    if not match:
+        return None
+    return match.group('id')
 
 
 def is_url(url):
