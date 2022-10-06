e6aa52cda8ea28c4c2f4bd95664d4d5ccbc5f5e1
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-10-06 22:26:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,38 +74,10 @@ def is_url(url):
     return False
 
 
-def format_selector(ctx):
-    """ Select the best video and the best audio that won't result in an mkv.
-    NOTE: This is just an example and does not handle all cases """
-
-    # acodec='none' means there is no audio
-    best_video = next(f for f in formats
-                      if f['vcodec'] != 'none' and f['acodec'] == 'none')
-
-    # find compatible audio extension
-    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
-    # vcodec='none' means there is no video
-    best_audio = next(f for f in formats if (
-        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
-
-    # These are the minimum required fields for a merged format
-    yield {
-        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
-        'ext': best_video['ext'],
-        'requested_formats': [best_video, best_audio],
-        # Must be + separated list of protocols
-        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
-    }
-
-
 async def download(video_url, bot_msg):
     info_file = 'info.json'
-    ydl_opts = {
-        'format': format_selector,
-    }
     output_filename = get_video_id(video_url) + '.webm'
-    print(ydl_opts)
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL() as ydl:
         thread = Thread(
             group=None,
             target=lambda: ydl.download([video_url])
